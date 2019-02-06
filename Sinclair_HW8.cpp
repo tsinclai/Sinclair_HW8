0d0c52833a0c24a246f6b26afdb61550667b3ad8
Tabitha Sinclair <tsinclai@my.loyno.edu>
2019-02-05 18:53:33 -0600

    Create Sinclair_HW8.cpp

---
+++ Sinclair_HW8.cpp
@@ -0,0 +1,51 @@
+/*
+All programming assignments for the remainder of this course must begin with
+a variation of the following four lines:
+
+Program Name: Stock Commission
+Purpose: Calculate stock prices based on user input.
+Author: Tabitha Sinclair
+Date Last Modified: 02-05-19
+*/
+ #include <iostream>
+using namespace std;
+
+int main () {
+
+/*
+double numberOfShares = 750;
+double pricePerShare = 35.00;
+double commissionToPay = .02;
+double amountForStock = numberOfShares * pricePerShare;
+double commissionEarned = amountForStock * commissionToPay;
+double totalPaid = amountForStock + commissionEarned;
+
+  cout << "The amount paid for the stock alone is $ " << amountForStock << endl;
+  cout << "The amount of the commission is $ " << commissionEarned << endl;
+  cout << "The total amount paid is $ " << totalPaid << endl;
+  return 0;
+*/
+double numberOfShares;
+double pricePerShare;
+double commissionToPay;
+
+
+ cout << "Enter Number of Shares ";
+ cin >> numberOfShares;
+ cout << "Enter Price per share $ ";
+ cin >> pricePerShare;
+ cout << "Enter the percent of commission to pay the stockbroker ";
+ cin >> commissionToPay;
+
+ double amountForStock = numberOfShares * pricePerShare;
+ double commissionEarned = amountForStock * commissionToPay;
+ double totalPaid = amountForStock + commissionEarned;
+
+ cout << "The amount paid for the stock alone is $ " << amountForStock << endl;
+ cout << "The amount of the commission is $ " << commissionEarned << endl;
+ cout << "The total amount paid is $ " << totalPaid << endl;
+ return 0;
+
+
+
+}
